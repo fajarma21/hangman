11d66d8a686567c5d2e8cd5e305c2af856403961
Fajar Maulana <fajarma21@gmail.com>
2026-04-22 08:23:09 +0700

    fix(input): change approach to support good experience on ipad

--- src/App/components/Input/View.tsx
+++ src/App/components/Input/View.tsx
@@ -1,30 +1,41 @@
-import type { FocusEvent } from 'react';
+import { useEffect, useRef } from 'react';
 
 import css from './View.module.scss';
 import type { InputProps } from './View.types';
 
+// NOTE: This approach produces a good experience on Ipad.
+
 const Input = ({ onFocusCallback, ...restProps }: InputProps) => {
   const { disabled, value } = restProps;
-  const handleForceFocus = (e: FocusEvent<HTMLInputElement>) => {
-    if (!disabled)
-      setTimeout(() => {
-        e.target.focus({
-          preventScroll: true,
-        });
-      }, 50);
+  const inputRef = useRef<HTMLInputElement>(null);
 
-    onFocusCallback();
-  };
+  useEffect(() => {
+    const handleFocus = (target: EventTarget | null) => {
+      if (inputRef.current && target !== inputRef.current && !disabled) {
+        inputRef.current.focus();
+        onFocusCallback();
+      }
+    };
+
+    document.addEventListener('click', ({ target }) => handleFocus(target));
+    return () =>
+      document.removeEventListener('click', ({ target }) =>
+        handleFocus(target),
+      );
+  }, [disabled, onFocusCallback]);
+
+  useEffect(() => {
+    if (inputRef.current) inputRef.current.focus();
+  }, []);
 
   return (
     <input
+      ref={inputRef}
       type="text"
       id="guess-letter"
       autoComplete="off"
-      autoFocus
       className={css.input}
       data-active={!!value || undefined}
-      onBlur={handleForceFocus}
       {...restProps}
     />
   );
